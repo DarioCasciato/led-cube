39278492fa9ae20a65992069bc65bbc58b02cffd
Dario Casciato <dario.casciato@dormakaba.com>
2024-02-22 09:09:59 +0100

    Fix LED color in movingLayers function

--- src/Modes/planes.cpp
+++ src/Modes/planes.cpp
@@ -6,12 +6,12 @@
 #include "Modes.h"
 #include "../Cube/Cube.h"
 #include "../utils/Timer/Timer.h"
- 
+
 namespace
 {
     static uint8_t cubeState[6][6][6] = {{{0}}};
 }
- 
+
 void Mode::movingLayers()
 {
     static Timer timer;
@@ -19,15 +19,15 @@ void Mode::movingLayers()
     static uint8_t verticalLayer = 0;
     static bool movingUp = true;
     static bool movingRight = true;
- 
+
     if (0 == timer.elapsedStart())
         timer.start();
- 
+
     if (timer.elapsed(500)) // Adjust the delay as needed for the speed of movement
     {
         // Reset the cubeState to zero (turn off all LEDs)
         memset(cubeState, 0, sizeof(cubeState));
- 
+
         // Set the LEDs in the horizontal layer to CYAN___
         for (uint8_t x = 0; x < 6; ++x)
         {
@@ -36,17 +36,17 @@ void Mode::movingLayers()
                 cubeState[horizontalLayer][x][y] = CYAN___;
             }
         }
- 
+
         // Set the LEDs in the vertical layer to CYAN___
         for (uint8_t z = 0; z < 6; ++z)
         {
-            cubeState[z][verticalLayer][horizontalLayer] = CYAN___; // This sets the intersecting vertical line
+            cubeState[z][verticalLayer][horizontalLayer] = GREEN__; // This sets the intersecting vertical line
             for (uint8_t x = 0; x < 6; ++x)
             {
                 cubeState[z][verticalLayer][x] = CYAN___;
             }
         }
- 
+
         // Update the horizontal layer position
         if (movingUp)
         {
@@ -68,7 +68,7 @@ void Mode::movingLayers()
                 movingUp = true;
             }
         }
- 
+
         // Update the vertical layer position
         if (movingRight)
         {
@@ -90,13 +90,13 @@ void Mode::movingLayers()
                 movingRight = true;
             }
         }
- 
-        
- 
+
+
+
         // Reset the timer
         timer.start();
     }
-    
+
     // Draw the cube with the current state of each layer
     CB::cube.drawCube(cubeState[0], cubeState[1], cubeState[2], cubeState[3], cubeState[4], cubeState[5]);
 }
